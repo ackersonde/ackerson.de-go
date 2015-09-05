e6bb3626b0d166e76ba019602b072da9cf496dee
Dan Ackerson <dan@ackerson.de>
2015-09-05 16:38:07 +0200

    try a different way

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 ssh core@ackerson.de <<-'ENDSSH'
 	docker pull blauerdrachen/ackerson.de:vc$CIRCLE_BUILD_NUM
-	docker stop ackerson.de && docker rm ackerson.de
-	docker run -d -p 80:3001 -v /opt:/opt -e ackSecret=$ackSecret -e ackPoems=$ackPoems -e ackWunder=$ackWunder -e ackMongo=$ackMongo --name ackerson.de blauerdrachen/ackerson.de:vc$CIRCLE_BUILD_NUM
+	if [ $? -eq 0 ]
+	then
+		docker stop ackerson.de && docker rm ackerson.de
+		docker run -d -p 80:3001 -v /opt:/opt -e ackSecret=$ackSecret -e ackPoems=$ackPoems -e ackWunder=$ackWunder -e ackMongo=$ackMongo --name ackerson.de blauerdrachen/ackerson.de:vc$CIRCLE_BUILD_NUM
+	fi
 ENDSSH
