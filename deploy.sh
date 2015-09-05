b06b5a7bec17fdc6a0a0e30985a48f5584ed392c
Dan Ackerson <dan@ackerson.de>
2015-09-05 09:54:31 +0200

    final deploy steps

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ssh core@ackerson.de <<-'ENDSSH'
-	touch /tmp/this
-	ls -lrt /tmp
+	docker pull blauerdrachen/ackerson.de:vc$CIRCLE_BUILD_NUM
+	docker stop ackerson.de && docker rm ackerson.de
+	docker run -d -p 80:3001 -v /opt:/opt -e ackSecret=$ackSecret -e ackPoems=$ackPoems -e ackWunder=$ackWunder -e ackMongo=$ackMongo --name ackerson.de blauerdrachen/ackerson.de:vc$CIRCLE_BUILD_NUM
 ENDSSH
