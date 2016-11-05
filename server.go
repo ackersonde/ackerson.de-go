6408773e6b878b31c7abb047b47ac4e326e2a55b
Dan Ackerson <dan@ackerson.de>
2016-11-05 10:17:50 +0100

    PlayBall homepage stays on Game 7, World Series until 2017 - Congrats, Cubbies

--- server.go
+++ server.go
@@ -203,6 +203,12 @@ var homePageMap map[int]baseball.Team
 func bbHome(w http.ResponseWriter, r *http.Request) {
 	date1 := r.URL.Query().Get("date1")
 	offset := r.URL.Query().Get("offset")
+
+	if date1 == "" {
+		//TODO 2016 is over - make default /bb goto Game 7, 2016 World Series
+		date1 = "year_2016/month_11/day_02"
+		offset = "0"
+	}
 	gameDayListing := baseball.GameDayListingHandler(date1, offset, homePageMap)
 
 	w.Header().Set("Cache-Control", "max-age=10800")
