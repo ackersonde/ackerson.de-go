a0149a5c77837139a0ccfae73188620a07c2a5ac
Dan Ackerson <dan@ackerson.de>
2018-03-31 15:12:00 +0200

    upgrade MLB API

--- baseball/baseball.go
+++ baseball/baseball.go
@@ -4,12 +4,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"sort"
 	"strconv"
 	"strings"
 	"time"
 
-	"github.com/clbanning/mxj"
+	"github.com/tidwall/gjson"
 )
 
 // FavoriteTeamGameListHandler is now commented
@@ -85,7 +86,8 @@ func getDatesAndGames(date1 string, offset string, homePageMap map[int]Team, fav
 		}
 	}
 
-	dates := "year_" + gameDate.Format("2006/month_01/day_02")
+	dates := gameDate.Format("01/02/2006")
+	//dates := "year_" + gameDate.Format("2006/month_01/day_02")
 	games := make(map[int][]string)
 	games = searchMLBGames(dates, games, homePageMap, favTeam)
 
@@ -94,183 +96,137 @@ func getDatesAndGames(date1 string, offset string, homePageMap map[int]Team, fav
 
 // SearchMLBGames is now commented
 func searchMLBGames(dates string, games map[int][]string, homePageMap map[int]Team, favTeam string) map[int][]string {
-	domain := "http://gd2.mlb.com/components/game/mlb/"
-	suffix := "/grid_ce.xml"
-	url := domain + dates + suffix
+	domain := "https://statsapi.mlb.com"
+	suffix := "/api/v1/schedule?sportId=1&date="
+	URL := domain + suffix + url.PathEscape(dates)
+	var awayTeamID, homeTeamID string
+	k := 0
 
 	startW1 := time.Now()
-	resp, err := http.Get(url)
+	resp, err := http.Get(URL)
 	if err != nil {
 		log.Print(err)
 		return nil
 	}
 	defer resp.Body.Close()
-	xml, err := ioutil.ReadAll(resp.Body)
+
+	raw, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Print(err)
+	}
+
+	scheduleJSON := string(raw)
 
 	elapsedW1 := time.Since(startW1)
-	log.Printf("%s took %s", url, elapsedW1)
+	log.Printf("%s took %s", URL, elapsedW1)
 
-	m, err := mxj.NewMapXml(xml)
+	gamesJSON := gjson.Get(scheduleJSON, "dates.0.games")
+	for _, game := range gamesJSON.Array() {
+		awayTeamID = game.Get("teams.away.team.id").String()
+		homeTeamID = game.Get("teams.home.team.id").String()
 
-	gameInfos, err := m.ValuesForKey("game")
-	if err != nil {
-		log.Fatal("err:", err.Error())
-		log.Printf("MLB site '%s' response empty", domain)
-		games[0] = []string{"Error connecting to " + domain}
-		return games
-	}
-
-	// now just manipulate Map entries returned as []interface{} array.
-	for k, v := range gameInfos {
-		gameID := ""
-		startX1 := time.Now()
-		aGameVal, _ := v.(map[string]interface{})
-		if aGameVal["-media_state"].(string) == "media_dead" {
-			continue
-		}
-		if favTeam != "" && // meaning we are looking for favTeam games
-			aGameVal["-away_team_id"].(string) != favTeam &&
-			aGameVal["-home_team_id"].(string) != favTeam {
+		if favTeam != "" && (favTeam != awayTeamID && favTeam != homeTeamID) {
+			//log.Printf("[%s]: %s != %s OR %s", dates, favTeam, awayTeamID, homeTeamID)
 			continue
 		}
 
-		// rescan looking for keys with data: Values or Value
-		gm := aGameVal["game_media"].(map[string]interface{})
-		hb := gm["homebase"].(map[string]interface{})
-		media := hb["media"].([]interface{})
-		for _, val := range media {
-			aMediaVal, _ := val.(map[string]interface{})
-			if aMediaVal["-type"].(string) != "condensed_game" {
-				continue
-			} else {
-				gameID = aMediaVal["-id"].(string)
-				continue
+		contentURL := game.Get("content.link").String()
+
+		if contentURL != "" {
+			awayTeam := LookupTeamInfo(homePageMap, awayTeamID)
+			homeTeam := LookupTeamInfo(homePageMap, homeTeamID)
+			gameURL := FetchGameURLFromID(contentURL)
+			if !strings.Contains(gameURL, "baseball.theater") {
+				gameID := game.Get("gamePK").String()
+				games[k] = []string{
+					awayTeam.Name, awayTeam.HomePage, strconv.Itoa(awayTeam.ID), awayTeam.Abbreviation,
+					homeTeam.Name, homeTeam.HomePage, strconv.Itoa(homeTeam.ID), homeTeam.Abbreviation,
+					gameID, dates, gameURL}
+				k++
 			}
 		}
-		elapsedX1 := time.Since(startX1)
-		log.Printf("XML parsing of subsection %s took %s", url, elapsedX1)
-
-		if gameID != "" {
-			awayTeamID := aGameVal["-away_team_id"].(string)
-			awayTeamName, awayTeamHomePage := LookupTeamInfo(homePageMap, awayTeamID)
-			awayAbbrev := aGameVal["-away_name_abbrev"].(string)
-
-			homeTeamID := aGameVal["-home_team_id"].(string)
-			homeTeamName, homeTeamHomePage := LookupTeamInfo(homePageMap, homeTeamID)
-			homeAbbrev := aGameVal["-home_name_abbrev"].(string)
-			gameURL := FetchGameURLFromID(gameID)
-
-			games[k] = []string{awayTeamName, awayTeamHomePage, awayTeamID, awayAbbrev, homeTeamName, homeTeamHomePage, homeTeamID, homeAbbrev, gameID, dates, gameURL}
-		}
 	}
-
 	return games
 }
 
 // FetchGameURLFromID is now commented
-func FetchGameURLFromID(gameID string) string {
-	detailURL := "http://m.mlb.com/gen/multimedia/detail" + generateDetailURL(gameID)
-
-	return fetchGameURL(detailURL, "FLASH_2500K_1280X720")
-}
-
-// fetchGameURL is now commented
-func fetchGameURL(detailURL string, desiredQuality string) string {
-	fallbackURL := "https://www.youtube.com/user/MLB"
-
-	startW1 := time.Now()
-	resp, err := http.Get(detailURL)
+func FetchGameURLFromID(contentURL string) string {
+	gameURL := "http://baseball.theater"
+	startX1 := time.Now()
+	resp, err := http.Get("https://statsapi.mlb.com" + contentURL)
 	if err != nil {
 		log.Print(err)
-	} else {
-		defer resp.Body.Close()
-
-		xml, err := ioutil.ReadAll(resp.Body)
-
-		elapsedW1 := time.Since(startW1)
-		log.Printf("%s took %s", detailURL, elapsedW1)
-
-		startX2 := time.Now()
-		m, err := mxj.NewMapXml(xml)
-
-		URLs, err := m.ValuesForKey("url")
-		if err != nil {
-			log.Fatal("err:", err.Error())
-			return ""
-		}
-
-		// now just manipulate Map entries returned as []interface{} array.
-		for _, v := range URLs {
-			aGameVal, _ := v.(map[string]interface{})
-			if aGameVal["-playback_scenario"].(string) == desiredQuality {
-				if aGameVal["#text"] != nil {
-					elapsedX2 := time.Since(startX2)
-					log.Printf("XML parsing of %s took %s", detailURL, elapsedX2)
-					return aGameVal["#text"].(string)
-				}
-				log.Print("ERROR: this game has no videoURL: " + detailURL)
-			}
-		}
 	}
 
-	return fallbackURL
-}
+	raw, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Print(err)
+	}
 
-// generateDetailURL is now commented
-func generateDetailURL(gameID string) string {
-	// given gameID 605442983 return "/9/8/3/605442983.xml"
-	return "/" + gameID[len(gameID)-3:len(gameID)-2] +
-		"/" + gameID[len(gameID)-2:len(gameID)-1] +
-		"/" + gameID[len(gameID)-1:] +
-		"/" + gameID + ".xml"
+	mediaJSON := string(raw)
+	video := gjson.Get(mediaJSON, `media.epgAlternate.#[title="Extended Highlights"].items.#.playbacks.#[name="FLASH_2500K_1280X720"].url`)
+
+	if len(video.Array()) <= 0 {
+		video = gjson.Get(mediaJSON, `media.epg.#[title="Extended Highlights"].items.#.playbacks.#[name="FLASH_2500K_1280X720"].url`)
+	}
+
+	elapsedX1 := time.Since(startX1)
+	log.Printf("JSON parsing of subsection %s took %s", contentURL, elapsedX1)
+
+	if len(video.Array()) > 0 {
+		gameURL = video.Array()[0].String()
+	}
+
+	return gameURL
 }
 
 // Team is now commented
 type Team struct {
-	ID             int
-	Name, HomePage string
+	ID                           int
+	Abbreviation, Name, HomePage string
 }
 
 // LookupTeamInfo is now commented
-func LookupTeamInfo(homePageMap map[int]Team, teamIDS string) (string, string) {
+func LookupTeamInfo(homePageMap map[int]Team, teamIDS string) Team {
 	teamID, _ := strconv.Atoi(teamIDS)
-	return homePageMap[teamID].Name, homePageMap[teamID].HomePage
+	return Team{teamID, homePageMap[teamID].Abbreviation, homePageMap[teamID].Name, homePageMap[teamID].HomePage}
 }
 
 // InitHomePageMap is now commented
 func InitHomePageMap() map[int]Team {
 	homePageMap := make(map[int]Team)
 
-	homePageMap[110] = Team{110, "Baltimore Orioles", "http://m.orioles.mlb.com/roster"}
-	homePageMap[145] = Team{145, "Chicago Whitesox", "http://m.whitesox.mlb.com/roster"}
-	homePageMap[117] = Team{117, "Houston Astros", "http://m.astros.mlb.com/roster"}
-	homePageMap[144] = Team{144, "Atlanta Braves", "http://m.braves.mlb.com/roster"}
-	homePageMap[112] = Team{112, "Chicago Cubs", "http://m.cubs.mlb.com/roster"}
-	homePageMap[109] = Team{109, "Arizona Diamond Backs", "http://m.dbacks.mlb.com/roster"}
-	homePageMap[111] = Team{111, "Boston Red Sox", "http://m.redsox.mlb.com/roster"}
-	homePageMap[114] = Team{114, "Cleveland Indians", "http://m.indians.mlb.com/roster"}
-	homePageMap[108] = Team{108, "Los Angeles Angels", "http://m.angels.mlb.com/roster"}
-	homePageMap[146] = Team{146, "Miami Marlins", "http://m.marlins.mlb.com/roster"}
-	homePageMap[113] = Team{113, "Cincinnati Reds", "http://m.reds.mlb.com/roster"}
-	homePageMap[115] = Team{115, "Colorado Rockies", "http://www.rockies.com/roster"}
-	homePageMap[147] = Team{147, "New York Yankees", "http://m.yankees.mlb.com/roster"}
-	homePageMap[116] = Team{116, "Detroit Tigers", "http://www.tigers.com/roster"}
-	homePageMap[133] = Team{133, "Oakland Athletics", "http://m.athletics.mlb.com/roster"}
-	homePageMap[121] = Team{121, "New York Mets", "http://m.mets.mlb.com/roster"}
-	homePageMap[158] = Team{158, "Milwaukee Brewers", "http://m.brewers.mlb.com/roster"}
-	homePageMap[119] = Team{119, "LA Dodgers", "http://m.dodgers.mlb.com/roster"}
-	homePageMap[139] = Team{139, "Tampa Bay Rays", "http://m.rays.mlb.com/roster"}
-	homePageMap[118] = Team{118, "Kansas City Royals", "http://m.royals.mlb.com/roster"}
-	homePageMap[136] = Team{136, "Seattle Mariners", "http://m.mariners.mlb.com/roster"}
-	homePageMap[143] = Team{143, "Philadelphia Phillies", "http://m.phillies.mlb.com/roster"}
-	homePageMap[138] = Team{138, "St Louis Cardinals", "http://m.cardinals.mlb.com/roster"}
-	homePageMap[135] = Team{135, "San Diego Padres", "http://m.padres.mlb.com/roster"}
-	homePageMap[141] = Team{141, "Toronto Blue Jays", "http://m.bluejays.mlb.com/roster"}
-	homePageMap[142] = Team{142, "Minnesota Twins", "http://m.twins.mlb.com/roster"}
-	homePageMap[140] = Team{140, "Texas Rangers", "http://m.rangers.mlb.com/roster"}
-	homePageMap[120] = Team{120, "Washington Nationals", "http://m.nationals.mlb.com/roster"}
-	homePageMap[134] = Team{134, "Pittsburgh Pirates", "http://m.pirates.mlb.com/roster"}
-	homePageMap[137] = Team{137, "San Francisco Giants", "http://m.giants.mlb.com/roster"}
+	homePageMap[110] = Team{110, "BAL", "Baltimore Orioles", "http://m.orioles.mlb.com/roster"}
+	homePageMap[145] = Team{145, "CHW", "Chicago Whitesox", "http://m.whitesox.mlb.com/roster"}
+	homePageMap[117] = Team{117, "HOU", "Houston Astros", "http://m.astros.mlb.com/roster"}
+	homePageMap[144] = Team{144, "ATL", "Atlanta Braves", "http://m.braves.mlb.com/roster"}
+	homePageMap[112] = Team{112, "CHC", "Chicago Cubs", "http://m.cubs.mlb.com/roster"}
+	homePageMap[109] = Team{109, "ARI", "Arizona Diamond Backs", "http://m.dbacks.mlb.com/roster"}
+	homePageMap[111] = Team{111, "BOS", "Boston Red Sox", "http://m.redsox.mlb.com/roster"}
+	homePageMap[114] = Team{114, "CLE", "Cleveland Indians", "http://m.indians.mlb.com/roster"}
+	homePageMap[108] = Team{108, "LAA", "Los Angeles Angels", "http://m.angels.mlb.com/roster"}
+	homePageMap[146] = Team{146, "MIA", "Miami Marlins", "http://m.marlins.mlb.com/roster"}
+	homePageMap[113] = Team{113, "CIN", "Cincinnati Reds", "http://m.reds.mlb.com/roster"}
+	homePageMap[115] = Team{115, "COL", "Colorado Rockies", "http://www.rockies.com/roster"}
+	homePageMap[147] = Team{147, "NYY", "New York Yankees", "http://m.yankees.mlb.com/roster"}
+	homePageMap[116] = Team{116, "DET", "Detroit Tigers", "http://www.tigers.com/roster"}
+	homePageMap[133] = Team{133, "OAK", "Oakland Athletics", "http://m.athletics.mlb.com/roster"}
+	homePageMap[121] = Team{121, "NYM", "New York Mets", "http://m.mets.mlb.com/roster"}
+	homePageMap[158] = Team{158, "MIL", "Milwaukee Brewers", "http://m.brewers.mlb.com/roster"}
+	homePageMap[119] = Team{119, "LAD", "LA Dodgers", "http://m.dodgers.mlb.com/roster"}
+	homePageMap[139] = Team{139, "TB", "Tampa Bay Rays", "http://m.rays.mlb.com/roster"}
+	homePageMap[118] = Team{118, "KC", "Kansas City Royals", "http://m.royals.mlb.com/roster"}
+	homePageMap[136] = Team{136, "SEA", "Seattle Mariners", "http://m.mariners.mlb.com/roster"}
+	homePageMap[143] = Team{143, "PHI", "Philadelphia Phillies", "http://m.phillies.mlb.com/roster"}
+	homePageMap[138] = Team{138, "STL", "St Louis Cardinals", "http://m.cardinals.mlb.com/roster"}
+	homePageMap[135] = Team{135, "SD", "San Diego Padres", "http://m.padres.mlb.com/roster"}
+	homePageMap[141] = Team{141, "TOR", "Toronto Blue Jays", "http://m.bluejays.mlb.com/roster"}
+	homePageMap[142] = Team{142, "MIN", "Minnesota Twins", "http://m.twins.mlb.com/roster"}
+	homePageMap[140] = Team{140, "TEX", "Texas Rangers", "http://m.rangers.mlb.com/roster"}
+	homePageMap[120] = Team{120, "WAS", "Washington Nationals", "http://m.nationals.mlb.com/roster"}
+	homePageMap[134] = Team{134, "PIT", "Pittsburgh Pirates", "http://m.pirates.mlb.com/roster"}
+	homePageMap[137] = Team{137, "SF", "San Francisco Giants", "http://m.giants.mlb.com/roster"}
 
 	return homePageMap
 }
